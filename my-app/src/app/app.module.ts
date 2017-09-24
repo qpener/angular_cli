e671fb21a57f34866b56c2ded8f3a061e03ad78e
qpener <tae4ja@hotmail.com>
2017-09-24 21:47:27 +0900

    Revert "git checkou"
    This reverts commit 338323ca334f9ce540b5d2b124891baf93ae824f.

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -14,9 +14,15 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule {
+<<<<<<< HEAD
   // first: String;
   second: String;
   thrid: String;
 }
 
 const test1 = new AppModule();
+=======
+    test: Number;
+ }
+
+>>>>>>> parent of 338323c... git checkou
